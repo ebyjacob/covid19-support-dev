eccf10e64746c3470e2668bf5ba2f05ca6175521
Sriramajeyam <yesoreyeram@gmail.com>
2020-04-01 03:38:35 +0100

    firebase config moved to config page

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const firebase_config = {
+    apiKey: "AIzaSyCVSv4vUrvdVXI6ZG-M-ec-OKMcGJbXLuU",
+    authDomain: "covid19-support-dev.firebaseapp.com",
+    databaseURL: "https://covid19-support-dev.firebaseio.com",
+    projectId: "covid19-support-dev",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
--- src/main.js
+++ src/main.js
@@ -1,22 +1,13 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import * as firebase from "firebase";
+import * as config from "./config/config";
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
-const configOptions = {
-  apiKey: "AIzaSyCVSv4vUrvdVXI6ZG-M-ec-OKMcGJbXLuU",
-  authDomain: "covid19-support-dev.firebaseapp.com",
-  databaseURL: "https://covid19-support-dev.firebaseio.com",
-  projectId: "covid19-support-dev",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
-
-firebase.initializeApp(configOptions);
+firebase.initializeApp(config.firebase_config);
 
 firebase.auth().onAuthStateChanged(user => {
   store.dispatch("fetchUser", user);
