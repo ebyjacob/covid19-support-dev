7cdc35cd36b6b66a29765c1bbcddbe80087352ac
Sriramajeyam <yesoreyeram@gmail.com>
2020-04-01 05:49:33 +0100

    My assigned Jobs in profile

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -8,7 +8,30 @@
           </div>
         </div>
         <div class="row mt-4">
-          <div class="col-sm-6"  v-if="mysupportrequests">
+          <div class="col-sm-12" v-if="myassignments">
+            <div class="card">
+              <div class="card-header">My Assignments</div>
+              <div class="card-body">
+                <div v-for="myassignment in myassignments" :key="myassignment.id">
+                  <span class="text-primary" style="font-size:16px;font-weight:bold;">
+                    <router-link
+                      :to="{ name: 'supportrequest', params: { supportrequestid: myassignment.id }}"
+                    >{{myassignment.data.request.title}}</router-link>
+                  </span>&nbsp;&nbsp;-&nbsp;&nbsp;
+                  <router-link
+                    :to="{ name: 'supportrequest', params: { supportrequestid: myassignment.id }}"
+                  >More details</router-link>
+                  <br />
+                  For {{ myassignment.data.contact.name || myassignment.data.contact.email }} by
+                  <i>{{myassignment.data.user_displayName || myassignment.data.user_email || 'Unknown User' }}</i>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-sm-6" v-if="mysupportrequests">
             <div class="card">
               <div class="card-header">Your Support Requests</div>
               <div class="card-body">
@@ -36,12 +59,10 @@
               <div class="card-header">Your Donation Requests</div>
               <div class="card-body">
                 <div v-for="donation in mydonations" :key="donation.id">
-                  <span
-                    class="text-primary"
-                    style="font-size:24px;font-weight:bold;"
-                  ><router-link :to="{ name: 'donation', params: { donationid: donation.id }}">
-                  {{donation.data.donation.title}}
-                  </router-link>
+                  <span class="text-primary" style="font-size:24px;font-weight:bold;">
+                    <router-link
+                      :to="{ name: 'donation', params: { donationid: donation.id }}"
+                    >{{donation.data.donation.title}}</router-link>
                   </span>
                   <br />
                   <i>{{donation.data.user_displayName || donation.data.user_email || 'Unknown User' }}</i>
@@ -70,37 +91,65 @@ export default {
   data() {
     return {
       mydonations: null,
-      mysupportrequests: null
+      mysupportrequests: null,
+      myassignments: null
     };
   },
+  methods: {
+    fetchDonationsList() {
+      var db = firebase.firestore();
+      db.collection("donations")
+        .where("user_email", "==", this.user.data.email)
+        .get()
+        .then(querySnapshot => {
+          let donations = [];
+          querySnapshot.forEach(doc => {
+            donations.push({
+              id: doc.id,
+              data: doc.data()
+            });
+          });
+          this.mydonations = donations;
+        });
+    },
+    fetchSupportRequests() {
+      var db = firebase.firestore();
+      db.collection("support_requests")
+        .where("user_email", "==", this.user.data.email)
+        .get()
+        .then(querySnapshot => {
+          let support_requests = [];
+          querySnapshot.forEach(doc => {
+            support_requests.push({
+              id: doc.id,
+              data: doc.data()
+            });
+          });
+          this.mysupportrequests = support_requests;
+        });
+    },
+    fetchMyAssigments() {
+      var db = firebase.firestore();
+      db.collection("support_requests")
+        .where("picked_up_by", "==", this.user.data.email)
+        .get()
+        .then(querySnapshot => {
+          console.log(querySnapshot);
+          let myassignments = [];
+          querySnapshot.forEach(doc => {
+            myassignments.push({
+              id: doc.id,
+              data: doc.data()
+            });
+          });
+          this.myassignments = myassignments;
+        });
+    }
+  },
   created() {
-    var db = firebase.firestore();
-    db.collection("donations")      
-      .where("user_email", "==", this.user.data.email)
-      .get()
-      .then(querySnapshot => {
-        let donations = [];
-        querySnapshot.forEach(doc => {
-          donations.push({
-            id: doc.id,
-            data: doc.data()
-          });
-        });
-        this.mydonations = donations;
-      });
-    db.collection("support_requests")
-      .where("user_email", "==", this.user.data.email)
-      .get()
-      .then(querySnapshot => {
-        let support_requests = [];
-        querySnapshot.forEach(doc => {
-          support_requests.push({
-            id: doc.id,
-            data: doc.data()
-          });
-        });
-        this.mysupportrequests = support_requests;
-      });
+    this.fetchDonationsList();
+    this.fetchSupportRequests();
+    this.fetchMyAssigments();
   },
   computed: {
     ...mapGetters({
