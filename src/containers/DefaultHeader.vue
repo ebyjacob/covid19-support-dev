a043749635048e4f86f317862c4b6e3c551b0cc9
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2020-04-06 22:24:08 +0100

    contact page url changed & testing cd pipeline

--- src/containers/DefaultHeader.vue
+++ src/containers/DefaultHeader.vue
@@ -46,7 +46,7 @@
         v-if="user.loggedIn && user.data && (user.data.admin || user.data.moderator || user.data.verifiedvolunteer)"
       >Groups</b-nav-item> -->
       <!-- <b-nav-item class="py-2 px-3" to="/about">About</b-nav-item>-->
-      <b-nav-item class="py-2 px-3" to="/contact">Contact us</b-nav-item> 
+      <b-nav-item class="py-2 px-3" to="/contact">Contact</b-nav-item> 
       <b-nav-item class="py-2 px-3" to="/contact/moderator" title="Contact Moderator"><i class="fa fa-envelope"></i></b-nav-item> 
     </b-navbar-nav>
     <b-navbar-nav class="ml-auto" style="margin-right:20px;">
