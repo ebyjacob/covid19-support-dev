54a964a7d24502388b67b1f8fe65de8d32502c10
Gnanasakthivel Ramanathan <gnanasakthivel.ramanathan@cognizant.com>
2020-04-07 22:55:56 +0100

    removed primary type validation

--- src/views/help/OfferSupport.vue
+++ src/views/help/OfferSupport.vue
@@ -565,7 +565,6 @@ export default {
         lastname: { required },
         mobile: { required },
         email: { required, email },
-        idtype: { required },
         idproof: { required }
       },
        address : {
