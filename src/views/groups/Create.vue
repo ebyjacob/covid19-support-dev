d54685fa322d194d755098dbed44aa1a48dc8db6
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2020-04-08 10:31:19 +0100

    vee-validate dependency fix

--- src/views/groups/Create.vue
+++ src/views/groups/Create.vue
@@ -69,7 +69,6 @@
 <script>
 import firebase from "firebase";
 import { mapGetters } from "vuex";
-import { ValidationProvider } from 'vee-validate';
 import { required, email, numeric } from "vuelidate/lib/validators";
 export default {
   data() {
